03b8bc6a258f302a46a218e7ae2f1109430a6845
PizzAna <anttikiviyo@gmail.com>
2014-07-09 21:32:01 +0300

    Should be like old now (wait what)

--- src/main/java/celestibytes/gradle/CelestiGradle.java
+++ src/main/java/celestibytes/gradle/CelestiGradle.java
@@ -1,5 +1,6 @@
 package celestibytes.gradle;
 
+import net.minecraftforge.gradle.CopyInto;
 import net.minecraftforge.gradle.common.BaseExtension;
 import net.minecraftforge.gradle.common.BasePlugin;
 import net.minecraftforge.gradle.delayed.DelayedBase;
@@ -7,21 +8,28 @@ import net.minecraftforge.gradle.delayed.DelayedFile;
 import net.minecraftforge.gradle.delayed.DelayedFileTree;
 import net.minecraftforge.gradle.delayed.DelayedString;
 import net.minecraftforge.gradle.tasks.abstractutil.DownloadTask;
+import net.minecraftforge.gradle.tasks.dev.ChangelogTask;
 
 import celestibytes.gradle.reference.Reference;
+import com.google.common.collect.Maps;
+import groovy.lang.Closure;
 import io.github.pizzana.util.GrepJava;
+import org.gradle.api.Action;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.java.archives.Manifest;
 import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.Delete;
+import org.gradle.api.tasks.bundling.Jar;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // TODO Replace that BaseExtension
 public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelayedResolver<BaseExtension>
@@ -41,7 +49,128 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
         if (projectName.equalsIgnoreCase(Reference.CW_NAME.toLowerCase()))
         {
             makeBaublesTask();
+            makeCWPackageTasks();
+            makeCWSignTask();
         }
+    }
+
+    public void makeCWPackageTasks()
+    {
+        String changelogFile = "{BUILD_DIR}/libs/" + project.getName() + "-" + project.getVersion() + "-changelog.txt";
+
+        ChangelogTask changelog = makeTask("createChangelog", ChangelogTask.class);
+
+        changelog.setServerRoot(delayedString("{JENKINS_SERVER}"));
+        changelog.setJobName(delayedString("{JENKINS_JOB}"));
+        changelog.setAuthName(delayedString("{JENKINS_AUTH_NAME}"));
+        changelog.setAuthPassword(delayedString("{JENKINS_AUTH_PASSWORD}"));
+        changelog.setTargetBuild(delayedString("{BUILD_NUM}"));
+        changelog.setOutput(delayedFile(changelogFile));
+        changelog.dependsOn("classes", "processResources");
+
+        project.getTasks().getByName("build").dependsOn(changelog);
+
+        Closure<Object> commonManifest = new Closure<Object>(project)
+        {
+            @Override
+            public Object call()
+            {
+                Manifest manifest = (Manifest) getDelegate();
+                manifest.getAttributes().put("FMLCorePlugin",
+                                             delayedString("celestialwizardry.codechicken.core.launch.DepLoader")
+                                                     .call());
+                manifest.getAttributes().put("FMLCorePluginContainsFMLMod", delayedString("true").call());
+                return null;
+            }
+        };
+
+        Jar jarTask = (Jar) project.getTasks().getByName("jar");
+        jarTask.manifest(commonManifest);
+        jarTask.dependsOn(changelog);
+
+        Jar javadoc = makeTask("javadocJar", Jar.class);
+        javadoc.setClassifier("javadoc");
+        javadoc.from(delayedFile("{BUILD_DIR}/docs/javadoc/"));
+        javadoc.dependsOn(jarTask, "javadoc");
+        javadoc.setExtension("jar");
+        project.getArtifacts().add("archives", javadoc);
+
+        Jar sources = makeTask("sourcesJar", Jar.class);
+        sources.setClassifier("sources");
+        sources.from(delayedFile(changelogFile));
+        sources.from(delayedFile("LICENSE"));
+        sources.from(delayedFile("build.gradle"));
+        sources.from(delayedFile("settings.gradle"));
+        sources.from(delayedFile("{BUILD_DIR}/sources/java/"));
+        sources.from(delayedFile("{BUILD_DIR}/resources/main/"));
+        sources.from(delayedFile("gradlew"));
+        sources.from(delayedFile("gradlew.bat"));
+        sources.from(delayedFile("gradle/wrapper"), new CopyInto("gradle/wrapper"));
+        sources.dependsOn(jarTask);
+        sources.setExtension("jar");
+        project.getArtifacts().add("archives", sources);
+
+        Jar api = makeTask("apiJar", Jar.class);
+        api.setClassifier("api");
+        api.from(delayedFile("{BUILD_DIR}/sources/java/celestialwizardry/api/"),
+                 new CopyInto("celestialwizardry/api/"));
+        api.from(delayedFile("{BUILD_DIR}/sources/java/celestialwizardry/crystal/api/"),
+                 new CopyInto("celestialwizardry/crystal/api/"));
+        api.dependsOn(jarTask);
+        api.setExtension("jar");
+        project.getArtifacts().add("archives", api);
+
+        Jar deobf = makeTask("deobfJar", Jar.class);
+        deobf.setClassifier("deobf");
+        sources.from(delayedFile(changelogFile));
+        sources.from(delayedFile("LICENSE"));
+        deobf.from(delayedFile("{BUILD_DIR}/classes/main/"));
+        deobf.from(delayedFile("{BUILD_DIR}/classes/api/"));
+        deobf.manifest(commonManifest);
+        deobf.dependsOn(jarTask);
+        deobf.setExtension("jar");
+        project.getArtifacts().add("archives", deobf);
+    }
+
+    public void makeCWSignTask()
+    {
+        final Jar jar = (Jar) project.getTasks().getByName("jar");
+        final File jarPath = jar.getArchivePath();
+        final File keystoreLocation = project.file(project.getProperties().get("keystore_location"));
+        final String keystoreAlias = (String) project.getProperties().get("keystore_alias");
+        final String keystorePassword = (String) project.getProperties().get("keystore_password");
+
+        DefaultTask signJar = makeTask("signJar", DefaultTask.class);
+        signJar.getInputs().file(jarPath);
+        signJar.getInputs().file(keystoreLocation);
+        signJar.getInputs().property("keystore_alias", keystoreAlias);
+        signJar.getInputs().property("keystore_password", keystorePassword);
+        signJar.getOutputs().file(jarPath);
+        signJar.onlyIf(new Spec<Task>()
+        {
+            @Override
+            public boolean isSatisfiedBy(Task task)
+            {
+                return !keystoreLocation.getPath().equals(".");
+            }
+        });
+        signJar.doLast(new Action<Task>()
+        {
+            @Override
+            public void execute(Task task)
+            {
+                Map<String, String> args = Maps.newHashMap();
+                args.put("destDir", jar.getDestinationDir().getPath());
+                args.put("jar", jarPath.getPath());
+                args.put("keystore", keystoreLocation.getPath());
+                args.put("alias", keystoreAlias);
+                args.put("storepass", keystorePassword);
+                project.getAnt().invokeMethod("signjar", args);
+            }
+        });
+        signJar.dependsOn("build");
+
+        project.getTasks().getByName("uploadArchives").dependsOn(signJar);
     }
 
     public void makeBaublesTask()
@@ -52,7 +181,8 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
             String baubles = GrepJava.getCWVersionFromJava(project, "BAUBLES");
             String baublesFile = "Baubles-deobf-" + baublesMc + "-" + baubles + ".jar";
             String baublesRoot = GrepJava.getPropertyFromJava(project,
-                                                              "src/main/java/celestialwizardry/reference/Reference.java",
+                                                              "src/main/java/celestialwizardry/reference/Reference" +
+                                                                      ".java",
                                                               "BAUBLES_ROOT");
             String baublesUrl = baublesRoot + baublesFile;
             final String baublesDest = "libs/" + baublesFile;
@@ -94,7 +224,8 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
                 cleanEveryBaubles.delete(delayedFile(baublesDest).call());
             }
 
-            cleanEveryBaubles.setDescription("Deletes all of the libraries containing \'Baubles\' in their name from the \'libs\' directory");
+            cleanEveryBaubles.setDescription(
+                    "Deletes all of the libraries containing \'Baubles\' in their name from the \'libs\' directory");
             cleanEveryBaubles.setGroup(Reference.NAME);
 
             Delete cleanBaubles = makeTask("cleanBaubles", Delete.class);
@@ -104,7 +235,9 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
                 cleanBaubles.delete(file);
             }
 
-            cleanBaubles.setDescription("Deletes all of the libraries containing \'Baubles\' in their name from the \'libs\' directory (excluding the up-to-date one)");
+            cleanBaubles.setDescription(
+                    "Deletes all of the libraries containing \'Baubles\' in their name from the \'libs\' directory " +
+                            "(excluding the up-to-date one)");
             cleanBaubles.setGroup(Reference.NAME);
 
             DownloadTask getBaubles = makeTask("getBaubles", DownloadTask.class);
@@ -121,6 +254,8 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
                     return excepted.call().exists() && !excepted.call().isDirectory();
                 }
             });
+            cleanBaubles.setDescription("Downloads the correct version of Baubles");
+            cleanBaubles.setGroup(Reference.NAME);
             getBaubles.dependsOn(cleanBaubles);
 
             project.getTasks().getByName("extractUserDev").dependsOn(getBaubles);

6b1d832dfc48a074ba22a7c415061f26014ab731
PizzAna <anttikiviyo@gmail.com>
2014-07-09 18:07:27 +0300

    Add references

---
+++ src/main/java/io/github/pizzana/cg/reference/Reference.java
@@ -0,0 +1,7 @@
+package io.github.pizzana.cg.reference;
+
+public class Reference
+{
+    public static final String NAME = "CelestiGradle";
+    public static final String VERSION = Version.VERSION;
+}

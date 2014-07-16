fbfd6b569f42a8429a2d2fb59bbafeef39565134
PizzAna <anttikiviyo@gmail.com>
2014-07-16 11:28:07 +0300

    Add core support

--- src/main/java/celestibytes/gradle/CelestiGradle.java
+++ src/main/java/celestibytes/gradle/CelestiGradle.java
@@ -46,11 +46,17 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
 
         // applyExternalPlugin("forge");
 
-        if (projectName.equalsIgnoreCase(Reference.CW_NAME.toLowerCase()))
+        if (projectName.toLowerCase().equals(Reference.CW_NAME.toLowerCase()))
         {
             makeBaublesTask();
             makeCWPackageTasks();
             makeCWSignTask();
+        }
+
+        if (projectName.toLowerCase().equals(Reference.CORE_NAME.toLowerCase()))
+        {
+            makeCorePackageTasks();
+            // TODO makeCoreSignTask();
         }
 
         makeLifecycleTasks();
@@ -136,6 +142,124 @@ public final class CelestiGradle implements Plugin<Project>, DelayedBase.IDelaye
     }
 
     public void makeCWSignTask()
+    {
+        final Jar jar = (Jar) project.getTasks().getByName("jar");
+        final File jarPath = jar.getArchivePath();
+        final File keystoreLocation = project.file(project.getProperties().get("keystore_location"));
+        final String keystoreAlias = (String) project.getProperties().get("keystore_alias");
+        final String keystorePassword = (String) project.getProperties().get("keystore_password");
+
+        DefaultTask signJar = makeTask("signJar", DefaultTask.class);
+        signJar.getInputs().file(jarPath);
+        signJar.getInputs().file(keystoreLocation);
+        signJar.getInputs().property("keystore_alias", keystoreAlias);
+        signJar.getInputs().property("keystore_password", keystorePassword);
+        signJar.getOutputs().file(jarPath);
+        signJar.onlyIf(new Spec<Task>()
+        {
+            @Override
+            public boolean isSatisfiedBy(Task task)
+            {
+                return !keystoreLocation.getPath().equals(".");
+            }
+        });
+        signJar.doLast(new Action<Task>()
+        {
+            @Override
+            public void execute(Task task)
+            {
+                Map<String, String> args = Maps.newHashMap();
+                args.put("destDir", jar.getDestinationDir().getPath());
+                args.put("jar", jarPath.getPath());
+                args.put("keystore", keystoreLocation.getPath());
+                args.put("alias", keystoreAlias);
+                args.put("storepass", keystorePassword);
+                project.getAnt().invokeMethod("signjar", args);
+            }
+        });
+        signJar.dependsOn("build");
+
+        project.getTasks().getByName("uploadArchives").dependsOn(signJar);
+    }
+
+    public void makeCorePackageTasks()
+    {
+        String changelogFile = "{BUILD_DIR}/libs/" + project.getName() + "-" + project.getVersion() + "-changelog.txt";
+
+        ChangelogTask changelog = makeTask("createChangelog", ChangelogTask.class);
+
+        changelog.setServerRoot(delayedString("{JENKINS_SERVER}"));
+        changelog.setJobName(delayedString("{JENKINS_JOB}"));
+        changelog.setAuthName(delayedString("{JENKINS_AUTH_NAME}"));
+        changelog.setAuthPassword(delayedString("{JENKINS_AUTH_PASSWORD}"));
+        changelog.setTargetBuild(delayedString("{BUILD_NUM}"));
+        changelog.setOutput(delayedFile(changelogFile));
+        changelog.dependsOn("classes", "processResources");
+
+        project.getTasks().getByName("build").dependsOn(changelog);
+
+        Closure<Object> commonManifest = new Closure<Object>(project)
+        {
+            @Override
+            public Object call()
+            {
+                Manifest manifest = (Manifest) getDelegate();
+                /* manifest.getAttributes().put("FMLCorePlugin",
+                                             delayedString(
+                                                     "celestibytes.celestialwizardry.codechicken.core.launch.DepLoader")
+                                                     .call());
+                manifest.getAttributes().put("FMLCorePluginContainsFMLMod", delayedString("true").call()); */
+                return null;
+            }
+        };
+
+        Jar jarTask = (Jar) project.getTasks().getByName("jar");
+        jarTask.manifest(commonManifest);
+        jarTask.dependsOn(changelog);
+
+        Jar javadoc = makeTask("javadocJar", Jar.class);
+        javadoc.setClassifier("javadoc");
+        javadoc.from(delayedFile("{BUILD_DIR}/docs/javadoc/"));
+        javadoc.dependsOn(jarTask, "javadoc");
+        javadoc.setExtension("jar");
+        project.getArtifacts().add("archives", javadoc);
+
+        Jar sources = makeTask("sourcesJar", Jar.class);
+        sources.setClassifier("sources");
+        sources.from(delayedFile(changelogFile));
+        sources.from(delayedFile("LICENSE"));
+        sources.from(delayedFile("build.gradle"));
+        sources.from(delayedFile("settings.gradle"));
+        sources.from(delayedFile("{BUILD_DIR}/sources/java/"));
+        sources.from(delayedFile("{BUILD_DIR}/resources/main/"));
+        sources.from(delayedFile("gradlew"));
+        sources.from(delayedFile("gradlew.bat"));
+        sources.from(delayedFile("gradle/wrapper"), new CopyInto("gradle/wrapper"));
+        sources.dependsOn(jarTask);
+        sources.setExtension("jar");
+        project.getArtifacts().add("archives", sources);
+
+        /* Jar api = makeTask("apiJar", Jar.class);
+        api.setClassifier("api");
+        api.from(delayedFile("{BUILD_DIR}/sources/java/celestibytes/core/api/"),
+        new CopyInto("celestibytes/core/api/"));
+        api.dependsOn(jarTask);
+        api.setExtension("jar"); */
+        // TODO project.getArtifacts().add("archives", api);
+
+        Jar deobf = makeTask("deobfJar", Jar.class);
+        deobf.setClassifier("deobf");
+        sources.from(delayedFile(changelogFile));
+        sources.from(delayedFile("LICENSE"));
+        deobf.from(delayedFile("{BUILD_DIR}/classes/main/"));
+        deobf.from(delayedFile("{BUILD_DIR}/classes/api/"));
+        deobf.manifest(commonManifest);
+        deobf.dependsOn(jarTask);
+        deobf.setExtension("jar");
+        project.getArtifacts().add("archives", deobf);
+    }
+
+    public void makeCoreSignTask()
     {
         final Jar jar = (Jar) project.getTasks().getByName("jar");
         final File jarPath = jar.getArchivePath();
--- src/main/java/celestibytes/gradle/reference/Reference.java
+++ src/main/java/celestibytes/gradle/reference/Reference.java
@@ -6,4 +6,5 @@ public class Reference
     public static final String VERSION = Version.VERSION;
 
     public static final String CW_NAME = "CelestialWizardry";
+    public static final String CORE_NAME = "CelestiCore";
 }

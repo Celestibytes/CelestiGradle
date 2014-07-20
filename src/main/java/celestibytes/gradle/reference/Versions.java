a84cfc30aa6e1339e50e968182c9415e7928a3b8
PizzAna <anttikiviyo@gmail.com>
2014-07-20 23:03:32 +0300

    bump version

--- src/main/java/celestibytes/gradle/reference/Versions.java
+++ src/main/java/celestibytes/gradle/reference/Versions.java
@@ -2,5 +2,5 @@ package celestibytes.gradle.reference;
 
 public final class Versions
 {
-    public static final String VERSION = "1.6-SNAPSHOT";
+    public static final String VERSION = "1.6.0-alpha.1";
 }

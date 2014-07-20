2d57398247cc187613203fb6c6f49f3cc55c7c7e
PizzAna <anttikiviyo@gmail.com>
2014-07-21 00:33:01 +0300

    Change default maven dir

--- src/main/java/celestibytes/gradle/CelestiGradlePlugin.java
+++ src/main/java/celestibytes/gradle/CelestiGradlePlugin.java
@@ -189,7 +189,7 @@ public final class CelestiGradlePlugin implements Plugin<Project>, DelayedBase.I
         }
         else
         {
-            filesmaven = ".";
+            filesmaven = "./files";
         }
 
         if (project.hasProperty("versionCheck"))

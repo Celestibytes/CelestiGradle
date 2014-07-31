914c035cffb48496b102195819ef071d9fdf9f4a
PizzAna <anttikiviyo@gmail.com>
2014-08-01 00:46:26 +0300

    Versions

--- src/main/java/celestibytes/gradle/reference/Versions.java
+++ src/main/java/celestibytes/gradle/reference/Versions.java
@@ -2,7 +2,7 @@ package celestibytes.gradle.reference;
 
 public final class Versions
 {
-    public static final String VERSION = "2.0.2";
+    public static final String VERSION = "2.0.3";
 
     public static final String DEFAULT_MINECRAFT_VERSION = "1.7.10";
 }

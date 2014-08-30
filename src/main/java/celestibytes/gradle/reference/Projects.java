37ce1a097d1f5d7e27f58c7fe425d290c49cbf42
PizzAna <anttikiviyo@gmail.com>
2014-08-30 17:57:47 +0300

    some constants

--- src/main/java/celestibytes/gradle/reference/Projects.java
+++ src/main/java/celestibytes/gradle/reference/Projects.java
@@ -5,5 +5,6 @@ public class Projects
     public static final String CW = "CelestialWizardry";
     public static final String DGC = "Doughcraft";
     public static final String CORE = "CelestiCore";
-    public static final String TT = "CelestiCore";
+    public static final String TT = "TankyTanks";
+    public static final String CF = "CelestiFarmer";
 }

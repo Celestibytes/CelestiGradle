f5bb37db1787a666bc913ab5467f82c6193883b7
PizzAna <anttikiviyo@gmail.com>
2014-09-06 17:20:07 +0300

    Testing version check

--- src/main/java/celestibytes/gradle/reference/Reference.java
+++ src/main/java/celestibytes/gradle/reference/Reference.java
@@ -20,7 +20,7 @@ public final class Reference
     public static final String NAME_FULL = "Celestibytes Gradle Plugin";
     public static final String VERSION = Versions.VERSION;
 
-    public static final String VERSION_CHECK_URL = "https://raw.githubusercontent.com/Celestibytes/CelestiGradle/master/version.txt";
+    public static final String VERSION_CHECK_URL = "https://raw.githubusercontent.com/Celestibytes/CelestiGradle/develop/version.txt";
 
     public static final String DEFAULT_DEPS_FILE = "dependencies.cbts";
 }

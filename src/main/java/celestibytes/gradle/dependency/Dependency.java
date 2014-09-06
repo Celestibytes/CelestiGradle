d556dae47183ac9cac01fa53eb4ebab024c34aaf
PizzAna <anttikiviyo@gmail.com>
2014-09-06 18:06:44 +0300

    Clean up

--- src/main/java/celestibytes/gradle/dependency/Dependency.java
+++ src/main/java/celestibytes/gradle/dependency/Dependency.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2014 Celestibytes
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -25,12 +25,12 @@ public final class Dependency
     private final String artifact;
     private final String version;
     private final List<String> aliases;
-
+    
     public Dependency(String group, String artifact, String version, String... aliases)
     {
         this(artifact, group, artifact, version, aliases);
     }
-
+    
     public Dependency(String name, String group, String artifact, String version, String... aliases)
     {
         this.name = name;
@@ -39,7 +39,7 @@ public final class Dependency
         this.version = version;
         this.aliases = Lists.newArrayList(aliases);
     }
-    
+
     /**
      * Gives the name of the {@link Dependency}.
      *
@@ -49,7 +49,7 @@ public final class Dependency
     {
         return name;
     }
-    
+
     /**
      * Gives the aliases of the {@link Dependency}.
      *
@@ -59,7 +59,7 @@ public final class Dependency
     {
         return aliases;
     }
-    
+
     /**
      * Gives the group of the {@link Dependency}.
      *
@@ -69,7 +69,7 @@ public final class Dependency
     {
         return group;
     }
-    
+
     /**
      * Gives the artifact of the {@link Dependency}.
      *
@@ -79,7 +79,7 @@ public final class Dependency
     {
         return artifact;
     }
-    
+
     /**
      * Gives the version of the {@link Dependency}.
      *
--- src/main/java/celestibytes/gradle/forge/gradle/DummyPlugin.java
+++ src/main/java/celestibytes/gradle/forge/gradle/DummyPlugin.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2014 Celestibytes
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -25,12 +25,12 @@ public class DummyPlugin extends BasePlugin<BaseExtension>
     public void applyPlugin()
     {
     }
-    
+
     @Override
     public void applyOverlayPlugin()
     {
     }
-    
+
     /**
      * return true if this plugin can be applied over another BasePlugin.
      */
@@ -39,13 +39,13 @@ public class DummyPlugin extends BasePlugin<BaseExtension>
     {
         return false;
     }
-    
+
     @Override
     protected DelayedFile getDevJson()
     {
         return null;
     }
-    
+
     /**
      * @return the extension object with name EXT_NAME_MC
      *
